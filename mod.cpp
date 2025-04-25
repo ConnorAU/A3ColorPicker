2c0da122e1bf82d08ef5ce31f1d11cdf3762be52
ConnorAU <15099385+ConnorAU@users.noreply.github.com>
2025-04-25 10:49:16 +1000

    Add mod.cpp Adding to git for use in remote build workflow

---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name="Color Picker";
+overview="Pick any color you like, with this color picker!";
+tooltipOwned="Color Picker";
+action="https://github.com/ConnorAU/A3ColorPicker";
+
+hideName=0;
+hidePicture=0;
+dlcColor[]={1,1,1,1};
+
+logo="\cau\colorpicker\icon.paa";
+logoOver="\cau\colorpicker\iconOver.paa";
+logoSmall="\cau\colorpicker\icon.paa";
